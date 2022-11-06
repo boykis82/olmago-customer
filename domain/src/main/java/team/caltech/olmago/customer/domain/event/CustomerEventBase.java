bfb383eaa243cbdf63db59f4563010c99e1338b3
boykis82 <boykis82@gmail.com>
2022-11-06 11:16:08 +0900

    2022-11-06 first commit

---
+++ domain/src/main/java/team/caltech/olmago/customer/domain/event/CustomerEventBase.java
@@ -0,0 +1,13 @@
+package team.caltech.olmago.customer.domain.event;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Getter
+public class CustomerEventBase {
+  protected final long customerId;
+  protected final LocalDateTime eventOccurDtm;
+}
